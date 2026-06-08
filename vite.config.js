b5c8ec60a10decaad201af3670e607556d3f1380
Moises Aguirre <moisesaguirre22@gmail.com>
2026-06-08 16:32:58 -0400

    Actualiza vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 changeOrigin: true,
             },
         },
+        historyApiFallback: true  // 🔥 ESTE ES EL FIX CLAVE
     },
     build: {
         outDir: 'dist'
