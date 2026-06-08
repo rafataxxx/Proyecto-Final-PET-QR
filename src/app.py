2146ecf1c11a7bb9e1491207232404d970cc347a
Rafael Mata <133695535+rafataxxx@users.noreply.github.com>
2026-06-08 11:08:22 -0400

    fix: mover sqlite a la carpeta tmp para permisos en render

--- src/app.py
+++ src/app.py
@@ -45,12 +45,8 @@ if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
         "postgres://", "postgresql://")
 else:
-    # 🛠️ SOLUCIÓN PARA RENDER: Generamos una ruta absoluta y creamos la carpeta instance de forma segura
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
-    os.makedirs(INSTANCE_DIR, exist_ok=True) # Forzamos la creación física de la carpeta
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(INSTANCE_DIR, 'mascotas.db')}"
+    # 🛠️ SOLUCIÓN PARA RENDER: Usamos la carpeta /tmp/ de Linux que SIEMPRE tiene permisos
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/mascotas.db"
 
 # Timeout para evitar bloqueos de hilos
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"connect_args": {"timeout": 30}}
